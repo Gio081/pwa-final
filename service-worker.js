3c948cae601791dd9fb5a466e070bfed6a7773ec
Giory01 <youvalencia89@gmai.com>
2025-10-03 00:31:36 -0600

    Versión inicial del proyecto PWA funcional

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'pwa-final-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Caché abierta');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
